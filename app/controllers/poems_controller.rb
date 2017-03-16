a7d75a4046070723d2d1c8e068cd08790130ca20
Psychedelicagogoparty <yosi@Yoshimitsu-no-MacBook-Air.local>
2017-03-16 20:53:42 +0900

    Api_poem ver1.0

--- app/controllers/poems_controller.rb
+++ app/controllers/poems_controller.rb
@@ -1,6 +1,14 @@
 class PoemsController < ApplicationController
+
   def index
     @poems = Poem.all
     render json: @poems
   end
+
+  def show
+    #@poem = Poem.find(1)
+    binding.pry
+    render json: @poem
+  end
+
 end
